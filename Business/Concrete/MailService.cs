f2da4717e8448dc0a00c2e55ffcc6c3dbb29483a
Ebubekir POLAT <ebubekirplt@gmail.com>
2024-01-10 11:11:52 +0300

    Sending mail operation moved into MailService

---
+++ Business/Concrete/MailService.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Core.Entities;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Business.Concrete
+{
+	public class MailService : IMailService
+	{
+		public void SendMailForResetPassword(AppUser user, string passwordResetTokenLink)
+		{
+			MimeMessage mimeMessage = new();
+			MailboxAddress mailboxAddressFrom = new("XYZ Social App", "xyzsocialapp@polattest.site");
+			mimeMessage.From.Add(mailboxAddressFrom);
+			MailboxAddress mailboxAddressTo = new(user.FirstName, "***REMOVED***");
+			mimeMessage.To.Add(mailboxAddressTo);
+			BodyBuilder bodyBuilder = new();
+			bodyBuilder.TextBody = $"Parolanızı sıfırlamak için linke tıklayın:  {passwordResetTokenLink}";
+			mimeMessage.Body = bodyBuilder.ToMessageBody();
+			mimeMessage.Subject = "Parola Sıfırlama Talebi";
+			SmtpClient client = new();
+			client.Connect("mail.polattest.site", 587, false);
+			client.Authenticate("xyzsocialapp@polattest.site", "***REMOVED***");
+			client.Send(mimeMessage);
+			client.Disconnect(true);
+		}
+	}
+}
