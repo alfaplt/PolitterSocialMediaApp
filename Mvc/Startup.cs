06f8675b5f84946eb50f19899df07c29a50357e5
Ebubekir POLAT <ebubekirplt@gmail.com>
2024-01-16 20:43:11 +0300

    Profile page link for users

--- Mvc/Startup.cs
+++ Mvc/Startup.cs
@@ -36,7 +36,7 @@ namespace Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation(); 
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
