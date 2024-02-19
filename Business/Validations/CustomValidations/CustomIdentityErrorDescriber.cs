f529532cf90028d5c9acb591c1bc0d3c23789525
Ebubekir POLAT <ebubekirplt@gmail.com>
2024-02-20 00:21:36 +0300

    Typo fixed.

--- Business/Validations/CustomValidations/CustomIdentityErrorDescriber.cs
+++ Business/Validations/CustomValidations/CustomIdentityErrorDescriber.cs
@@ -10,7 +10,7 @@ namespace Business.Validations.CustomValidations
 	public class CustomIdentityErrorDescriber : IdentityErrorDescriber
 	{
 		public override IdentityError DuplicateUserName(string userName) => new IdentityError { Code = "DuplicateUserName", Description = $"\"{userName}\" başka bir kullanıcı tarafından kullanılmaktadır." };
-		public override IdentityError InvalidUserName(string userName) => new IdentityError { Code = "InvalidUserName", Description = "Geçersiz kullanıcı adı. Türkçe karakter mi kullandın yoksa? :)" };
+		public override IdentityError InvalidUserName(string userName) => new IdentityError { Code = "InvalidUserName", Description = "Lütfen kullanıcı adı oluştururken Türkçe karakter(ı, İ, ü, ğ, ş, ö, ç) kullanmayınız. Özel karakterlerden ise yalnızca birkaçını (-, ., _, @, +) kullanabilirsiniz." };
 		public override IdentityError DuplicateEmail(string email) => new IdentityError { Code = "DuplicateEmail", Description = $"\"{email}\" başka bir kullanıcı tarafından kullanılmaktadır." };
 		public override IdentityError InvalidEmail(string email) => new IdentityError { Code = "InvalidEmail", Description = "Geçersiz email." };
 	}
